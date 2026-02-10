24b80732a2c99c571461dce48386e43f0513db91
roman <roman.klyucharov@gmail.com>
2026-02-10 16:38:09 +0300

    test data

---
+++ test_data.py
@@ -0,0 +1,21 @@
+from db.db import Session, init_db
+from db.db_models import Customer, Order
+
+if __name__ == '__main__':
+    init_db()
+    with Session() as session:
+        customer = Customer(name="Данис", surname="Абдреев", phone=79872378827, secret_key="ZGFuaXNfcGlkcg==")
+        session.add(customer)
+        session.commit()
+
+        order = Order(customer_id=customer.id)
+        session.add(order)
+        session.commit()
+
+        customer = Customer(name="Роман", surname="Ключаров", phone=79520313144, secret_key="11111")
+        session.add(customer)
+        session.commit()
+
+        order = Order(customer_id=customer.id)
+        session.add(order)
+        session.commit()
