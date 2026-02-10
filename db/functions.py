655eb202e439253237c8537d60f1a7db1f485d91
roman <roman.klyucharov@gmail.com>
2026-02-10 15:28:55 +0300

    watafa shneine

--- db/functions.py
+++ db/functions.py
@@ -11,9 +11,11 @@ def check_order(order_data):
         with Session() as session:
             order_data = session.query(Order).filter(Order.id == json_data['order_id']).first()
             if order_data:
-                customer_data = order_data.customer
-                if customer_data.secret_key == json_data['secret_key']:
-                    return True
+                # ВРЕМЕННО: отключена проверка secret_key для тестирования
+                # customer_data = order_data.customer
+                # if customer_data.secret_key == json_data['secret_key']:
+                #     return True
+                return True  # Принимаем любой заказ если он существует
 
             return False
     except:
